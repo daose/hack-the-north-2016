fc0d5083d13723d2582f254a52881ae556211f1e
daose <bychen@uwaterloo.ca>
2016-09-17 14:02:30 -0400

    put keys in

--- app/src/main/java/com/daose/hackthenorth/MainActivity.java
+++ app/src/main/java/com/daose/hackthenorth/MainActivity.java
@@ -17,11 +17,29 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final String API_HOST = "api.yelp.com";
+    private static final String DEFAULT_TERM = "dinner";
+    private static final String DEFAULT_LOCATION = "San Francisco, CA";
+    private static final int SEARCH_LIMIT = 3;
+    private static final String SEARCH_PATH = "/v2/search";
+    private static final String BUSINESS_PATH = "/v2/business";
+
+    /*
+     * Update OAuth credentials below from the Yelp Developers API site:
+     * http://www.yelp.com/developers/getting_started/api_access
+     */
+    private static final String CONSUMER_KEY = "_pSsOl9dQIh4898P-kUoig";
+    private static final String CONSUMER_SECRET = "ZNGrKazE9JEN2qxenYAPf2Rj0iM";
+    private static final String TOKEN = "NrQ-3hTN62cPgeItv8b5otNIJiTkkpQ6";
+    private static final String TOKEN_SECRET = "36y1ayki63KnTocDWrS-tZfWqXk";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, UClipService.class);
         startService(intent);
+
+
     }
 }
