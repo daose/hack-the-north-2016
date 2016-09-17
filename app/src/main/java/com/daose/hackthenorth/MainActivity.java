a4976a5a1c3f764cb0c4859f2e23e6b98e644727
daose <bychen@uwaterloo.ca>
2016-09-17 01:06:02 -0400

    added test copy

--- app/src/main/java/com/daose/hackthenorth/MainActivity.java
+++ app/src/main/java/com/daose/hackthenorth/MainActivity.java
@@ -1,7 +1,10 @@
 package com.daose.hackthenorth;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView view = (TextView) findViewById(R.id.text);
+
+        final ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        clipboard.addPrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
+            @Override
+            public void onPrimaryClipChanged() {
+                String copiedText = "";
+                if (clipboard.hasPrimaryClip()) {
+                    ClipData data = clipboard.getPrimaryClip();
+                    if (data.getItemCount() > 0) {
+                        copiedText = data.getItemAt(0).coerceToText(MainActivity.this).toString();
+                    }
+                }
+                view.setText(copiedText);
+            }
+        });
+
     }
 }
